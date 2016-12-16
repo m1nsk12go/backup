9ae4f8de26208f65f1933cab21dd39167e0cf610
Dim kop <westhawk@ya.ru>
2016-12-16 09:48:22 +0300

    cron, install

---
+++ loops.bash
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# for f in $(ls /); do
+	# echo $f
+# done
+
+# for f in $(ls .); do rm -rf $f done
+
+# COUNT=7
+# while [ $COUNT -gt 0 ]; do
+	# echo $COUNT
+	
+	# let COUNT=COUNT-1
+	# COUNT=$(( $COUNT-1 ))
+# done
+
+
+# for (( i=0; i < 10; i++)); do
+	# echo $i
+# done
+
+# for i in 5 18 19 21 16; do
+	# echo $i
+# done
+
+# for i in $(seq 1 .2 2); do
+	# echo $i
+# done
+
+# for (( i=0, j=10; i !=j; i++,j--)); do
+	# echo $(( i*j ))
+# done
+
+# for i in 5 18 19 21 16; do
+	# if [ $i -gt 10 -a $i -lt 20 ]; then 
+		# continue
+	# fi
+	# echo $i
+# done
